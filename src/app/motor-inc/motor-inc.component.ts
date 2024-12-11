6930c58640dd7596e827fb8606ea8452a12b18df
codeng22 <codeng22@gmail.com>
2024-12-11 08:10:24 +0530

    Motor inc code changes

---
+++ src/app/motor-inc/motor-inc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motor-inc',
+  templateUrl: './motor-inc.component.html',
+  styleUrls: ['./motor-inc.component.css']
+})
+export class MotorIncComponent {
+
+  public sampleData:any ="Hello Sample Data";
+
+}
